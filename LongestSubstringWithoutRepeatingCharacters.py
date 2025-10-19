fa55753959f5f7bcd6cf2bc2ea5dc514748c98eb
emirhan <147313740+eemirhankar54@users.noreply.github.com>
2025-10-19 23:24:09 +0300

    Update LongestSubstringWithoutRepeatingCharacters.py

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -2,13 +2,24 @@ def lengthOfLongestSubstring(s):
     sol = 0
     en_uzun = 0
     karakterler = set()
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        sol = 0
+        en_uzun = 0
+        karakterler = set()
 
     for sag in range(len(s)):
         while s[sag] in karakterler:
             karakterler.remove(s[sol])
             sol += 1
+        for sag in range(len(s)):
+            while s[sag] in karakterler:
+                karakterler.remove(s[sol])
+                sol += 1
 
         karakterler.add(s[sag])
         en_uzun = max(en_uzun, sag - sol + 1)
+            karakterler.add(s[sag])
+            en_uzun = max(en_uzun, sag - sol + 1)
 
-    return en_uzun
+    return en_uzun        return en_uzun
