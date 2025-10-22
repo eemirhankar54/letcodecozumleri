70809929244cb833ce6f24dcf3aa7c2252756c40
emirhan <147313740+eemirhankar54@users.noreply.github.com>
2025-10-22 10:05:52 +0300

    Create FindTheIndexofTheFirstOccurrenceInAStrin.py

---
+++ FindTheIndexofTheFirstOccurrenceInAStrin.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        if needle == "":
+            return 0
+        if len(needle) > len(haystack):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i 
+        return -1 
+
